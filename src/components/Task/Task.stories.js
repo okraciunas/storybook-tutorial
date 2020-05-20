63c23b06c29f457209757edd6a64182a678275ef
Leonardo Oliva Kraciunas <leonardo@okraciunas.com>
2020-05-20 17:04:57 -0300

    Cria componente Task.

---
+++ src/components/Task/Task.stories.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { action } from "@storybook/addon-actions";
+
+import Task, { TaskState } from "./";
+
+export default {
+  component: Task,
+  title: "Task",
+  excludeStories: /.*Data$/,
+};
+
+export const taskData = {
+  id: "1",
+  title: "Test Task",
+  state: TaskState.TASK_INBOX,
+  updatedAt: new Date(2018, 0, 1, 9, 0),
+};
+
+export const actionsData = {
+  onPinTask: action("onPinTask"),
+  onArchiveTask: action("onArchiveTask"),
+};
+
+export const Default = () => <Task task={{ ...taskData }} {...actionsData} />;
+
+export const Pinned = () => (
+  <Task task={{ ...taskData, state: TaskState.TASK_PINNED }} {...actionsData} />
+);
+
+export const Archived = () => (
+  <Task
+    task={{ ...taskData, state: TaskState.TASK_ARCHIVED }}
+    {...actionsData}
+  />
+);
---
+++ src/components/Task/index.js
@@ -0,0 +1,56 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const TaskState = {
+  TASK_PINNED: "TASK_PINNED",
+  TASK_INBOX: "TASK_INBOX",
+  TASK_ARCHIVED: "TASK_ARCHIVED",
+};
+
+export default function Task({
+  task: { id, title, state },
+  onArchiveTask,
+  onPinTask,
+}) {
+  return (
+    <div className={`list-item ${state}`}>
+      <label className="checkbox">
+        <input
+          type="checkbox"
+          defaultChecked={state === TaskState.TASK_ARCHIVED}
+          disabled={true}
+          name="checked"
+        />
+        <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
+      </label>
+
+      <div className="title">
+        <input
+          type="text"
+          value={title}
+          readOnly={true}
+          placeholder="Input title"
+        />
+      </div>
+
+      <div className="actions" onClick={(event) => event.stopPropagation()}>
+        {state !== TaskState.TASK_ARCHIVED && (
+          // eslint-disable-next-line jsx-a11y/anchor-is-valid
+          <a onClick={() => onPinTask(id)}>
+            <span className="icon-star" />
+          </a>
+        )}
+      </div>
+    </div>
+  );
+}
+
+Task.propTypes = {
+  task: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    state: PropTypes.string.isRequired,
+  }),
+  onArchiveTask: PropTypes.func,
+  onPinTask: PropTypes.func,
+};
